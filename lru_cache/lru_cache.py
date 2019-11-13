e46db4702e5adc18dbb5d810ff2f0b25ab986143
Jamie Goodnight <goodnightlambda@gmail.com>
2019-11-13 15:40:12 -0500

    :construction: LRU hadn't been pushed. Added code to binary search tree

--- lru_cache/lru_cache.py
+++ lru_cache/lru_cache.py
@@ -47,8 +47,8 @@ class DoublyLinkedList:
 
     def __len__(self):
         return self.length
-    """Wraps the given value in a ListNode and inserts it 
-    as the new head of the list. Don't forget to handle 
+    """Wraps the given value in a ListNode and inserts it
+    as the new head of the list. Don't forget to handle
     the old head node's previous pointer accordingly."""
 
     def add_to_head(self, value):
@@ -69,8 +69,8 @@ class DoublyLinkedList:
         value = self.head.value
         self.delete(self.head)
         return value
-    """Wraps the given value in a ListNode and inserts it 
-    as the new tail of the list. Don't forget to handle 
+    """Wraps the given value in a ListNode and inserts it
+    as the new tail of the list. Don't forget to handle
     the old tail node's next pointer accordingly."""
 
     def add_to_tail(self, value):
@@ -84,7 +84,7 @@ class DoublyLinkedList:
             self.tail.next = new_node
             self.tail = new_node
 
-    """Removes the List's current tail node, making the 
+    """Removes the List's current tail node, making the
     current tail's previous node the new tail of the List.
     Returns the value of the removed Node."""
 
@@ -92,7 +92,7 @@ class DoublyLinkedList:
         value = self.tail.value
         self.delete(self.tail)
         return value
-    """Removes the input node from its current spot in the 
+    """Removes the input node from its current spot in the
     List and inserts it as the new head node of the List."""
 
     def move_to_front(self, node):
@@ -101,7 +101,7 @@ class DoublyLinkedList:
         value = node.value
         self.delete(node)
         self.add_to_head(value)
-    """Removes the input node from its current spot in the 
+    """Removes the input node from its current spot in the
     List and inserts it as the new tail node of the List."""
 
     def move_to_end(self, node):
@@ -182,4 +182,16 @@ class LRUCache:
     """
 
     def set(self, key, value):
-        pass
+        if key in self.storage:
+            node = self.storage[key]
+            self.ddl.move_to_end(node)
+            node.value = (key, value)
+        else:
+            if self.size == self.limit:
+                del self.storage[self.ddl.head.value[0]]
+                self.ddl.remove_from_head()
+                self.size -= 1
+
+        self.ddl.add_to_tail((key, value))
+        self.storage[key] = self.ddl.tail
+        self.size += 1
